daa24750fb39c8a56e309f89f6fd2d4afa42bdce
Artem Pivnev <ArtemixArtemix@yandex.ru>
2025-01-22 22:32:06 +0300

    Отправка на ревью 6 спринта (первая попытка)

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -4,6 +4,17 @@ import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
+    private static class Node {
+        Task task;
+
+        Node prev;
+
+        Node next;
+
+        Node(Task task) {
+            this.task = task;
+        }
+    }
     private final Map<Integer, Node> taskMap = new HashMap<>();
 
     private int size = 0;
