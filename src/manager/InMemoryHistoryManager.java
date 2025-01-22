8832104850c47ba02e7cc3cb5bf436db924b62c1
Artem Pivnev <ArtemixArtemix@yandex.ru>
2025-01-22 22:10:22 +0300

    Отправка на ревью 6 спринта (первая попытка)

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -1,11 +1,16 @@
 package manager;
 import model.Task;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private List<Task> history = new LinkedList<>();
+    private final Map<Integer, Node> taskMap = new HashMap<>();
+
+    private int size = 0;
+
+    private Node head;
+
+    private Node tail;
 
     private static final int MAX_HISTORY_SIZE = 10;
 
@@ -16,19 +21,65 @@ public class InMemoryHistoryManager implements HistoryManager {
             return;
         }
 
-        Task task;
-        task = new Task(newTask.getNameTask(), newTask.getDescription(), newTask.getStatusTask());
+        Task task  = new Task(newTask.getNameTask(), newTask.getDescription(), newTask.getStatusTask());
         task.setId(newTask.getId());
 
-        history.add(task);
+        Node newNode = new Node(task);
 
-        if (history.size() > MAX_HISTORY_SIZE) {
-            history.remove(0);
+        linkLast(newNode);
+        taskMap.put(newNode.task.getId(), newNode);
+
+        if (size > MAX_HISTORY_SIZE) {
+            removeNode(head);
+        }
+    }
+
+    public void removeNode(Node node) {
+        if (node == null) {
+            return;
+        }
+        if (node.prev != null) {
+            node.prev.next = node.next;
+        } else {
+            head = node.next;
+        }
+        if (node.next != null) {
+            node.next.prev = node.prev;
+        } else {
+            tail = node.prev;
+        }
+        taskMap.remove(node.task.getId());
+        size--;
+    }
+
+    public void linkLast(Node node) {
+        if (tail == null) {
+            head = node;
+            tail = node;
+        }
+        else {
+            tail.next = node;
+            node.prev = tail;
+            tail = node;
+        }
+        size++;
+    }
+
+    @Override
+    public void remove(int id) {
+        if (taskMap.containsKey(id)) {
+            removeNode(taskMap.get(id));
         }
     }
 
     @Override
     public List<Task> getHistory() {
-        return new LinkedList<>(history);
+        List<Task> historyList = new ArrayList<>();
+        Node current = head;
+        while (current != null) {
+            historyList.add(current.task);
+            current = current.next;
+        }
+        return historyList;
     }
 }
